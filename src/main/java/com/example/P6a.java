f417c11501e8d329cd85f2238ecd92bbb501d331
Deekshith <Deekshith@Eshrush>
2026-04-19 18:59:56 +0530

    commit

---
+++ src/main/java/com/example/P6a.java
@@ -0,0 +1,48 @@
+/*6a. Build a servlet program to find the factorial of a number using HTML with step by step procedure.*/
+package com.example;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldSecond
+ */
+@WebServlet("/HelloWorldSecond")
+public class P6a extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public P6a() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	PrintWriter out = response.getWriter();
+        	out.println("<html><body>");
+        	out.println("<h2 style='color:blue;'>Hello World!!!</h2>");
+        	out.println("<p style='color:magenta;'>The server time is: " + new java.util.Date() + "</p>");
+        	out.println("</body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
+
