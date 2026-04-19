b17d78c37acaa0247f9367201778f391c7169298
DhanyaVA <dhanyavadhanya@gmail.com>
2026-04-19 15:24:27 +0530

    Add files via upload

---
+++ q5.java
@@ -0,0 +1,13 @@
+package String_Functions;
+
+public class q5 {
+
+	    public static String removeWhitespace(String str) {
+	        return str == null ? null : str.replaceAll("\\s", "");
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println("Result: " + removeWhitespace("Hello World"));
+	    }
+	}
+
