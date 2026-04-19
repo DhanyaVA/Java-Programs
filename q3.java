009c05a8827bcf1cf33cba5b9e84f941c7d62a51
DhanyaVA <dhanyavadhanya@gmail.com>
2026-04-19 15:20:01 +0530

    Add files via upload

---
+++ q3.java
@@ -0,0 +1,14 @@
+package String_Functions;
+
+public class q3 {
+
+	    public static String reverseString(String str) {
+	        if (str == null) return null;
+	        return new StringBuilder(str).reverse().toString();
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println("Reverse: " + reverseString("hello"));
+	    }
+	}
+
